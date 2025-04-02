985491d0bdb4e53e58a878a266a34ad105d5410b
Arash avan <arash.avan@gmail.com>
2025-04-02 12:24:21 +0330

    new models
    setting new models in the project

--- blog/models.py
+++ blog/models.py
@@ -4,5 +4,14 @@ from django.db import models
 
 
 class post(models.Model):
+    # image
+    # author
     title = models.CharField(max_length=225)
     content = models.TextField()
+    # tag
+    # category
+    counted_views = models.IntegerField(default=0)  # default=0
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
