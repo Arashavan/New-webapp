279971f6266a79447d3f0dcecf1a73124c12f141
Arash avan <arash.avan@gmail.com>
2025-04-10 18:35:48 +0330

    contact admin

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from website.models import contact
 # Register your models here.
+
+
+class ContactAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    list_display = ('name', 'subject', 'created_date', 'email',)
+    list_filter = ('name',)
+
+
+admin.site.register(contact,ContactAdmin)
