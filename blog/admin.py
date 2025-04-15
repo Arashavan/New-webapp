51a3c97a5f141f641597fe84beb833ba3fb3d950
Arash avan <arash.avan@gmail.com>
2025-04-15 19:19:52 +0330

    updating blog and templates

--- blog/admin.py
+++ blog/admin.py
@@ -6,7 +6,7 @@ from blog.models import post
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
-    fields = ('title', 'published_date', 'status',)
+    # fields = ('title', 'published_date', 'status',)
     list_display = ('title', 'status', 'created_date', 'counted_views')
     list_filter = ('status',)
 
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-
+from blog.models import post
 # Create your views here.
 
 
 def blog_view(request):
-    return render(request, 'blog/blog-home.html')
+    posts = post.objects.filter(status=1)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
 
 
 def blog_single(request):
     return render(request, 'blog/blog-single.html')
+
+
+def test(request):
+    posts = post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'test.html', context)
