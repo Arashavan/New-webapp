d5ef227012e8798a6a10fb22abd904f506f7e2ef
Arash avan <arash.avan@gmail.com>
2025-04-28 09:30:44 +0330

    def comment

--- blog/views.py
+++ blog/views.py
@@ -11,6 +11,20 @@ def blog_view(request):
 
 
 def blog_single(request, pid):
+    """
+    This Python function retrieves a single blog post based on the provided ID, increments the view
+    count for that post, and renders a template with the post data.
+    
+    :param request: The `request` parameter in the `blog_single` function is an HttpRequest object that
+    represents the current HTTP request. It contains metadata about the request, such as headers,
+    method, and user data. This parameter is typically passed to Django view functions to process and
+    respond to incoming requests
+    :param pid: The `pid` parameter in the `blog_single` function is typically used to identify the
+    specific post that the user wants to view. It is usually the primary key (pk) of the post in the
+    database. This function retrieves the post with the given `pid` from the database, increments the
+    :return: The `blog_single` view function is returning a rendered template `blog/blog-single.html`
+    with the context containing the specific post retrieved based on the `pid` parameter.
+    """
     posts = Post.objects.filter(status=1)
     post = get_object_or_404(posts, pk=pid)
     post.counted_view += 1
