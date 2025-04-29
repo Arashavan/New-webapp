cf701505738e88df418f0d6de113f43cd5712ed8
Arash avan <arash.avan@gmail.com>
2025-04-29 12:19:52 +0330

    setting next and previous posts in blog single

--- blog/views.py
+++ blog/views.py
@@ -11,25 +11,23 @@ from django.utils import timezone
 
 
 def blog_single(request, pid):
-    """
-    This Python function retrieves a single blog post based on the provided ID, increments the view
-    count for that post, and renders a template with the post data.
-
-    :param request: The `request` parameter in the `blog_single` function is an HttpRequest object that
-    represents the current HTTP request. It contains metadata about the request, such as headers,
-    method, and user data. This parameter is typically passed to Django view functions to process and
-    respond to incoming requests
-    :param pid: The `pid` parameter in the `blog_single` function is typically used to identify the
-    specific post that the user wants to view. It is usually the primary key (pk) of the post in the
-    database. This function retrieves the post with the given `pid` from the database, increments the
-    :return: The `blog_single` view function is returning a rendered template `blog/blog-single.html`
-    with the context containing the specific post retrieved based on the `pid` parameter.
-    """
-    posts = Post.objects.filter(status=1)
+    posts = Post.objects.filter(
+        status=1, published_date__lte=timezone.now()).order_by('published_date')
     post = get_object_or_404(posts, pk=pid)
+
     post.counted_view += 1
     post.save(update_fields=['counted_view'])
-    context = {'post': post}
+
+    next_post = posts.filter(published_date__gt=post.published_date).order_by(
+        'published_date').first()
+    prev_post = posts.filter(published_date__lt=post.published_date).order_by(
+        '-published_date').first()
+
+    context = {
+        'post': post,
+        'next_post': next_post,
+        'prev_post': prev_post,
+    }
     return render(request, 'blog/blog-single.html', context)
 
 
