439762c613a0f6896b9a7d106958d3b17271f37b
Arash avan <arash.avan@gmail.com>
2025-05-15 12:21:04 +0330

    forms update

--- website/forms.py
+++ website/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 
 
-class Nameform(forms.From):
-    name = forms.CharField(label="name", max_length=255)
+class Nameform(forms.Form):
+    Name = forms.CharField(label="Name", max_length=255)
+    email = forms.EmailField()
+    subject = forms.CharField(max_length=225)
+    message = forms.CharField(widget=forms.Textarea)
--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from website.models import Contact
+from website.forms import Nameform
 
 # Create your views here.
 from django.http import HttpResponse, JsonResponse
@@ -19,17 +20,28 @@ def contact_view(request):
 
 def test(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        subject = request.POST.get('subject')
-        message = request.POST.get('message')
-        c = Contact(
-            name=name,
-            email=email,
-            subject=subject,
-            message=message
-        )
-        c.save()
-        print(name, email, subject, message)
+        form = Nameform(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.changed_data['subject']
+            message = form.cleaned_data['message']
+            print(name, email, subject, message)
+            return HttpResponse('done')
+        else:
+            return HttpResponse('not valid')
 
-    return render(request, 'test.html')
+            # name = request.POST.get('name')
+            # email = request.POST.get('email')
+            # subject = request.POST.get('subject')
+            # message = request.POST.get('message')
+            # c = Contact(
+            #     name=name,
+            #     email=email,
+            #     subject=subject,
+            #     message=message
+            # )
+            # c.save()
+
+    form = Nameform()
+    return render(request, 'test.html', {'form': form})
